90fee074738d6723ee91d9e2013be3063ff4ae48
Loi Dinh <loiphatdinh@gmail.com>
2023-02-02 10:15:19 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     //'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-    'mongoUrl' : 'mongodb://127.0.0.1'
+    'mongoUrl' : 'mongodb://127.0.0.1/nucampsite', 
+    'facebook': {
+        clientId: '897332504750671',
+        clientSecret: 'b25538062f9a8147d36c234ccd5d40bf'
+    }
 }
